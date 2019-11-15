0fcad291903ab738fc9a40e00e54fa33b58807e2
kyuno053 <nicolaslef53@gmail.com>
2019-11-15 23:41:59 +0100

    feature : add gsManagement function

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ bot.on('message', function (user, userID, channelID, message, event) {
             debugProcess(user, userID, channelID, message);
             break;
         case config.operator.command:
-
+            commandProcess(user, userID, channelID, message);
             break;
     }
 
@@ -44,6 +44,67 @@ function debugProcess(user, userID, channelID, message) {
     }
 
 
+}
+
+function commandProcess(user, userID, channelID, message) {
+
+    let args = message.slice(1,message.length ).split(' ');
+
+    switch (args[0]) {
+
+        case "gs":
+            gsManagement(user, userID, channelID,args);
+        break;
+    }
+
+
+}
+
+function gsManagement(user, userID, channelID,args){
+
+    if(args.length > 1){
+
+        let mobs = null;
+        switch (args[1]) {
+
+            case "addNat5Def":
+               mobs = args[2].split('/');
+               if (mobs.length > 3){
+                   bot.sendMessage({
+                       to:channelID,
+                       message: "```css\n#gs your team is too long!\n```"
+                   });
+               }else{
+                   bot.sendMessage({
+                       to:channelID,
+                       message: "```css\n#gs your team\n```\n```diff\n- "+mobs[0]+" "+mobs[1]+" "+mobs[2]+"\n```"
+                   });
+               }
+
+                break;
+            case "addNat4Def":
+                mobs= args[2].split('/');
+                if (mobs.length > 3){
+                    bot.sendMessage({
+                        to:channelID,
+                        message: "```css\n#gs your team is too long!\n```"
+                    });
+                }else{
+                    bot.sendMessage({
+                        to:channelID,
+                        message: "```css\n#gs your team\n```\n```fix\n- "+mobs[0]+" "+mobs[1]+" "+mobs[2]+"\n```"
+                    });
+                }
+
+                break;
+
+        }
+    }else{
+        bot.sendMessage({
+            to:channelID,
+            message: "```css\n#gs Missing parameter(s)\n```"
+        });
+    }
 }
 
 
