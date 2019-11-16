7a97f4a8271fc183237ad46a3f17fe71013c1af3
kyuno053 <nicolaslef53@gmail.com>
2019-11-16 23:24:27 +0100

    feature: add functions to register GS defs

--- bot.js
+++ bot.js
@@ -1,13 +1,21 @@
 let Discord = require("discord.io");
 const token = require("./config/auth").token;
 const config = require("./config/general");
+let userData = null;
+let fs = require('fs');
 let bot = new Discord.Client({
     token: token,
     autorun: true
 });
 
+let userList = [];
+
+
 bot.on('ready', function () {
-    console.log("Bot ready!");
+    console.log("Bot ready! - connected: "+bot.connected);
+    userList = bot.users;
+    generateUsers();
+    userData = require("./defs/defSiege");
 });
 bot.on('message', function (user, userID, channelID, message, event) {
 
@@ -30,7 +38,15 @@ bot.on('message', function (user, userID, channelID, message, event) {
 bot.connect();
 
 
-//functions
+/**
+ *
+ * @param user
+ * @param userID
+ * @param channelID
+ * @param message
+ */
+
+
 function debugProcess(user, userID, channelID, message) {
 
     let args = message.slice(1,message.length ).split(' ');
@@ -46,6 +62,14 @@ function debugProcess(user, userID, channelID, message) {
 
 }
 
+/**
+ *
+ * @param user
+ * @param userID
+ * @param channelID
+ * @param message
+ */
+
 function commandProcess(user, userID, channelID, message) {
 
     let args = message.slice(1,message.length ).split(' ');
@@ -59,6 +83,14 @@ function commandProcess(user, userID, channelID, message) {
 
 
 }
+
+/**
+ *
+ * @param user
+ * @param userID
+ * @param channelID
+ * @param args
+ */
 
 function gsManagement(user, userID, channelID,args){
 
@@ -75,10 +107,18 @@ function gsManagement(user, userID, channelID,args){
                        message: "```css\n#gs your team is too long!\n```"
                    });
                }else{
-                   bot.sendMessage({
-                       to:channelID,
-                       message: "```css\n#gs your team\n```\n```diff\n- "+mobs[0]+" "+mobs[1]+" "+mobs[2]+"\n```"
-                   });
+                   if(!testDefDupes(mobs,userID)){
+                       saveDef5Siege(mobs,userID);
+                       bot.sendMessage({
+                           to:channelID,
+                           message: "```css\n#gs your team\n```\n```diff\n- "+mobs[0]+" "+mobs[1]+" "+mobs[2]+"\n```\n```fix\n+ Team successfully added! \n```"
+                       });
+                   }else{
+                       bot.sendMessage({
+                           to:channelID,
+                           message: "```css\n#gs your team is already registered!\n```"
+                       });
+                   }
                }
 
                 break;
@@ -90,10 +130,18 @@ function gsManagement(user, userID, channelID,args){
                         message: "```css\n#gs your team is too long!\n```"
                     });
                 }else{
-                    bot.sendMessage({
-                        to:channelID,
-                        message: "```css\n#gs your team\n```\n```fix\n- "+mobs[0]+" "+mobs[1]+" "+mobs[2]+"\n```"
-                    });
+                    if(!testDefDupes(mobs,userID)){
+                        saveDef4Siege(mobs,userID);
+                        bot.sendMessage({
+                            to:channelID,
+                            message: "```css\n#gs your team\n```\n```fix\n- "+mobs[0]+" "+mobs[1]+" "+mobs[2]+"\n```\n```fix\n+ Team successfully added! \n```"
+                        });
+                    }else{
+                        bot.sendMessage({
+                            to:channelID,
+                            message: "```css\n#gs your team is already registered!\n```"
+                        });
+                    }
                 }
 
                 break;
@@ -107,4 +155,125 @@ function gsManagement(user, userID, channelID,args){
     }
 }
 
+/**
+ *
+ * @param args
+ * @param userId
+ */
+
+function saveDef4Siege(args,userId){
+    let data;
+    àç
+    for (let i=0;i<args.length;i++){
+         data = {first:args[0],second:args[1],third:args[2],isStrong:false};
+    }
+    for(let user of userData.users){
+        if(user.userId === userId){
+           user.siege.nat4def.push(data);
+        }
+    }
+    fs.writeFile(config.path.defSiege,JSON.stringify(userData),function (err) {
+        if(err) throw err;
+    });
+}
+
+
+/**
+ *
+ * @param args
+ * @param userId
+ */
+
+function saveDef5Siege(args,userId){
+    let data;
+
+    for (let i=0;i<args.length;i++){
+        data = {first:args[0],second:args[1],third:args[2],isStrong:false};
+    }
+    for(let user of userData.users){
+        if(user.userId === userId){
+            user.siege.nat5def.push(data);
+        }
+    }
+    fs.writeFile(config.path.defSiege,JSON.stringify(userData),function (err) {
+        if(err) throw err;
+    });
+}
+
+
+/**
+ *
+ *
+ *
+ */
+
+function generateUsers() {
+
+    if(!config.userLock){
+        let data = {users:[]};
+        for (let user in userList){
+            if(!userList[user].bot){
+                data.users.push({userId:userList[user].id,username:userList[user].username,siege:{nat5def:[],nat4def:[],lock:false}});
+            }
+        }
+        fs.writeFile(config.path.defSiege,JSON.stringify(data),function (err) {
+            if(err) throw err;
+
+        });
+        config.userLock = true;
+          fs.writeFile("./config/general.json",JSON.stringify(config),function (err) {
+          if(err) throw err;
+      });
+
+    }
+
+
+
+}
+
+/**
+ *
+ * @param args
+ * @param userId
+ * @returns {boolean}
+ */
+
+function testDefDupes(args,userId){
+    let isDupe = false;
+    for(let user of userData.users){
+        if(user.userId === userId){
+           for (let def4 of user.siege.nat4def){
+             if(def4.first === args[0] && def4.second===args[1] && def4.third ===args[2]){
+                 isDupe = true;
+             }
+           }
+           for (let def5 of user.siege.nat5def){
+               if(def5.first === args[0] && def5.second===args[1] && def5.third ===args[2]){
+                   isDupe = true;
+               }
+           }
+
+        }
+    }
+return isDupe;
+}
+
+/**
+ *
+ * @param userId
+ */
+
+function isLocked(userId) {
+   let isLocked = false;
+
+    for(let user of userData.users){
+        if(user.userId === userId){
+           if(user.siege.nat5def.length === config.userDefLockParam.nb5nat && user.siege.nat4def.length === config.userDefLockParam.nb4nat){
+               isLocked = true;
+           }
+
+        }
+    }
+
+}
 
